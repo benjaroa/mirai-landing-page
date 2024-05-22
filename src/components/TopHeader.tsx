445edb07e5d21efe0aff4ac5d69dcc2f5f64dc4e
Benja Roa <benjaroa@gmail.com>
2024-05-22 03:54:10 -0400

    corrige traducción

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -4,12 +4,12 @@ export const TopHeader = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="container">
+    <div className="container mb-4">
       <div className="flex items-start justify-between py-4">
         <a className="flex items-center space-x-2">
           <div className="flex flex-col space-y-1 text-sm leading-none">
-            <span className="font-bold text-xl">Mirai Food Lab</span>
-            <span className="text-muted-foreground">
+            <span className="font-bold text-4xl">Mirai Food Lab</span>
+            <span className="text-muted-foreground text-xl">
               {t("hero.main-text")}
             </span>
           </div>
