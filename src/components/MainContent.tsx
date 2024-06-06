301564dbd71ec88fce828f248e13ec9bfe46fd54
Benja Roa <benjaroa@gmail.com>
2024-06-06 19:30:02 -0400

    modifica contenido principal

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -17,20 +17,85 @@ type Project = {
 const processLinks = (links: Link[]) => {
   return links.map(({ target, title, type, className }, key) => {
     const component = typesMap[type] || getFallback;
-    return component({ key, target, title, className });
+    return (
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        {component({ key, target, title, className })}
+      </span>
+    );
   });
 };
 
 export const MainContent = () => {
   const { t } = useTranslation();
   return (
-    <div className="container col-span-2 grid items-start gap-6 lg:col-span-1">
+    <div className="container grid grid-cols-1 gap-4 md:grid-cols-3">
       {(t("projects", { returnObjects: true }) as Project[]).map(
         ({ title, description, links }, index: number) => (
           <div
             key={`project-${index}`}
-            className=" grid grid-cols-3 gap-4"
+            className="rounded overflow-hidden shadow"
           >
+            <img
+              className="w-full"
+              src="/assets/hero-1.jpg"
+              alt="Sunset in the mountains"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{title}</div>
+              <p className="text-gray-700 text-base">{description}</p>
+            </div>
+            <div className="px-6 pt-4 pb-2">{processLinks(links)}</div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+/*
+          <div key={`project-${index}`} className="card ">
+            <div className="max-w-sm w-full lg:max-w-full lg:flex">
+              <div
+                className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                style={{backgroundImage: `url('/assets/hero-1.jpg')`}}
+                title="Woman holding a mug"
+              ></div>
+              <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                <div className="mb-8">
+                  <p className="text-sm text-gray-600 flex items-center">
+                    <svg
+                      className="fill-current text-gray-500 w-3 h-3 mr-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                    </svg>
+                    Members only
+                  </p>
+                  <div className="text-gray-900 font-bold text-xl mb-2">
+                    {title}
+                  </div>
+                  <p className="text-gray-700 text-base">
+                    {description}
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    className="w-10 h-10 rounded-full mr-4"
+                    src="/img/jonathan.jpg"
+                    alt="Avatar of Jonathan Reinink"
+                  />
+                  <div className="text-sm">
+                    <p className="text-gray-900 leading-none">
+                    {processLinks(links)}
+                    </p>
+                    <p className="text-gray-600">Aug 18</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          //////////////////////////////////
             <div className="">
               <img
                 className="object-cover object-center h-60"
@@ -46,8 +111,4 @@ export const MainContent = () => {
               <div className="align-bottom">{processLinks(links)}</div>
             </div>
           </div>
-        )
-      )}
-    </div>
-  );
-};
+*/
