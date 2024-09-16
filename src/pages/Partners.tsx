b2244f1675b08e721a56f6f8002e8afaff178cb7
Benja Roa <benjaroa@gmail.com>
2024-09-16 14:16:08 -0300

    corrige gap en mobile

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -145,7 +145,7 @@ export const Partners = () => {
               </div>
             </div>
 
-            <div className="grid grid-cols-0 gap-0 sm:gap-5 sm:grid-cols-2">
+            <div className="grid grid-cols-0 gap-4 sm:grid-cols-2">
               {(partnersList as CustomCardType[]).map((partner, i) => (
                 <CustomCard key={`${name}-${i}`} {...partner} />
               ))}
