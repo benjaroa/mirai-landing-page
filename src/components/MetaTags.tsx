fe375482a68a9ffb96e6e5b9bd28d8eaf13939ff
Benja Roa <benjaroa@gmail.com>
2024-09-17 03:07:12 -0300

    corrige helmet

--- src/components/MetaTags.tsx
+++ src/components/MetaTags.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
+import { Fragment } from "react/jsx-runtime";
 
 export const MetaTags = () => {
   const { t, i18n: { language } } = useTranslation();
@@ -15,22 +16,24 @@ export const MetaTags = () => {
   };
 
   return (
-    <Helmet>
-      <meta name="description" content={description} />
-      
-      <meta property="og:locale" content={language} />"
-      <meta property="og:description" lang={language} content={description} />
-      <meta property="og:title" content={ogData.title} />
-      <meta property="og:url" content={ogData.url} />
-      <meta property="og:image" content={ogData.image} />
-      <meta property="og:logo" content={ogData.logoUrl} />
-      <meta property="og:site_name" content={ogData.siteName} />
-      <meta property="og:type" content="business.business" />
+    <Fragment>
+      <Helmet key="react-helmet">
+        <meta name="description" content={description} />
+        
+        <meta property="og:locale" content={language} />"
+        <meta property="og:description" lang={language} content={description} />
+        <meta property="og:title" content={ogData.title} />
+        <meta property="og:url" content={ogData.url} />
+        <meta property="og:image" content={ogData.image} />
+        <meta property="og:logo" content={ogData.logoUrl} />
+        <meta property="og:site_name" content={ogData.siteName} />
+        <meta property="og:type" content="business.business" />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={ogData.title} />
-      <meta name="twitter:description" lang={language} content={description} />
-      <meta name="twitter:image" content={ogData.image} />
-    </Helmet>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={ogData.title} />
+        <meta name="twitter:description" lang={language} content={description} />
+        <meta name="twitter:image" content={ogData.image} />
+      </Helmet>
+    </Fragment>
   )
 }
