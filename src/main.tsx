b91f5c6b8dd93e0e77d40da82ff1a00e320aa76f
Benja Roa <benjaroa@gmail.com>
2024-09-23 03:23:53 -0300

    elimina helmet y agrega los tags a mano

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./i18n.ts";
 import { Router } from "./Router.tsx";
-import { MetaTags } from "./components/MetaTags.tsx";
+// import { MetaTags } from "./components/MetaTags.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MetaTags />
+    {/* <MetaTags /> */}
     <Router />
   </React.StrictMode>
 );
