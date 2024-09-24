209088b320f3b9fc079953a0b886de5fc259cf36
Benja Roa <benjaroa@gmail.com>
2024-09-24 03:52:14 -0300

    mejoras en el rooteo

--- src/components/LocalesToggle.tsx
+++ src/components/LocalesToggle.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { Link } from "wouter";
 
 const baseClasses = "text-sm h-8 w-8 border";
 const selectedClass = "font-bold bg-slate-600 text-white";
@@ -8,20 +9,28 @@ export const LocaleToggle = () => {
   const { i18n } = useTranslation();
   return (
     <div className="inline-flex">
-      <button
-        className={`${baseClasses} ${i18n.language === "es" ? selectedClass : unSelectedClass} rounded-l border-r-0`}
-        value={"es"}
-        onClick={() => i18n.changeLanguage("es")}
-      >
-        ES
-      </button>
-      <button
-        className={`${baseClasses} ${i18n.language === "en" ? selectedClass : unSelectedClass} rounded-r`}
-        value={"en"}
-        onClick={() => i18n.changeLanguage("en")}
-      >
-        EN
-      </button>
+      <Link to="/es">
+        <button
+          className={`${baseClasses} ${
+            i18n.language === "es" ? selectedClass : unSelectedClass
+          } rounded-l border-r-0`}
+          value={"es"}
+          onClick={() => i18n.changeLanguage("es")}
+        >
+          ES
+        </button>
+      </Link>
+      <Link to="/en">
+        <button
+          className={`${baseClasses} ${
+            i18n.language === "en" ? selectedClass : unSelectedClass
+          } rounded-r`}
+          value={"en"}
+          onClick={() => i18n.changeLanguage("en")}
+        >
+          EN
+        </button>
+      </Link>
     </div>
   );
 };
