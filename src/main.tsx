a9a6b424472415612e1e01fbf80590209a3d1eba
Benja Roa <benjaroa@gmail.com>
2024-09-24 04:04:59 -0300

    agrega googla analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./i18n.ts";
 import { Router } from "./Router.tsx";
-// import { MetaTags } from "./components/MetaTags.tsx";
+import ReactGA from "react-ga4";
+// impor t { MetaTags } from "./components/MetaTags.tsx";
+
+if (process.env.NODE_ENV !== "development") {
+  ReactGA.initialize("G-GGNKDSPYZ3");
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
